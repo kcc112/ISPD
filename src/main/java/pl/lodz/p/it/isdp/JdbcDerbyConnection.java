d8f21524a602ed45afa4e757f8f55d8cdc42ba85
Kamil Celejewski <kamil12283@gmail.com>
2019-12-25 11:57:52 +0100

    [Add] save value as clob

--- src/main/java/pl/lodz/p/it/isdp/JdbcDerbyConnection.java
+++ src/main/java/pl/lodz/p/it/isdp/JdbcDerbyConnection.java
@@ -1,20 +1,25 @@
 package pl.lodz.p.it.isdp;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
+import javax.sql.rowset.serial.SerialClob;
 
 public class JdbcDerbyConnection {
     static void connect() {
-        try {
-            String dbURL3 = "jdbc:derby://localhost:1527/xd";
-            Properties properties = new Properties();
-            properties.put("create", "true");
-            Connection conn3 = DriverManager.getConnection(dbURL3,properties);
-            if (conn3 != null) {
-                System.out.println("Connected to database #3");
-            }
+        String connectionStrings = "jdbc:derby://localhost:1527/test1";
+        Properties properties = new Properties();
+        properties.put("user", "admin");
+        properties.put("password", "123");
+
+        String sql = "INSERT INTO array (Array) VALUES (?)";
+
+        try (Connection conn = DriverManager.getConnection(connectionStrings, properties);
+             PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+
+            Clob clob = new SerialClob("Kamil".toCharArray());
+            preparedStatement.setClob(1, clob);
+            preparedStatement.executeUpdate();
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
